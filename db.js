eab302c31cff7f25d4f9d3237bec2f21c9d09f9c
Nikitiyy <nikita184615@gmail.com>
2026-05-29 17:22:28 +0300

    feat: install dotenv and change db password

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'Nikitiy',
-    password: 'Vf61zgjS',
-    database: 'web_tech',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,    
+    connectionLimit: 10,
     queueLimit: 0
-})
+});
 
 module.exports = pool;
